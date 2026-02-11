3cf6a67740bde7a61e86b637fc458724b8346bb4
nguyendinhdo148 <nguyendinhdo2k7.com>
2026-02-11 09:07:38 +0700

    1

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,21 +6,25 @@ import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
 import chatRoutes from "./routes/chat.route.js";
-
 import cors from "cors";
-
 import * as Sentry from "@sentry/node";
 
 const app = express();
 
+// CONNECT DB (chạy 1 lần)
+connectDB();
+
+// MIDDLEWARE
 app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // req.auth will be available in the request object
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(clerkMiddleware());
 
-app.get("/debug-sentry", (req, res) => {
-  throw new Error("My first Sentry error!");
-});
-
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Hello World! 123");
 });
@@ -28,22 +32,8 @@ app.get("/", (req, res) => {
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 
+// SENTRY
 Sentry.setupExpressErrorHandler(app);
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () => {
-        console.log("Server started on port:", ENV.PORT);
-      });
-    }
-  } catch (error) {
-    console.error("Error starting server:", error);
-    process.exit(1); // Exit the process with a failure code
-  }
-};
-
-startServer();
-
+// ❌ KHÔNG listen
 export default app;
